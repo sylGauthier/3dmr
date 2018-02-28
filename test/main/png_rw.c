1998ec17c19ad7a91a32704af2f5587e6829a8eb
dok <dok@grehack.fr>
2018-02-28 14:38:33 +0100

    test: add png test program
    The png_rw program is for testing reading and writting png image.
    It read and then write an image.  After run this program the output
    image must be the same as the input image.

---
+++ test/main/png_rw.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../src/png.h"
+
+int png_read_write(const char *in, const char *out, unsigned int alignRow) {
+    unsigned int width, height;
+    int alpha;
+    void *buffer;
+    int retval;
+
+    retval = png_read(in, alignRow, &width, &height, &alpha, &buffer);
+    if (!retval) {
+	return EXIT_FAILURE;
+    }
+
+    retval = png_write(out, alignRow, width, height, alpha, 0, buffer);
+    if (!retval) {
+	return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+	puts("Usage: png_rw <infile> <outfile>");
+	return EXIT_FAILURE;
+    }
+
+    return png_read_write(argv[1], argv[2], 0);
+}
