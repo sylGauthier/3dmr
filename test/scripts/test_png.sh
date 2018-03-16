c1c5b4b8478df24ad8a2a7fd943fd9808fed50ff
François-Xavier Carton <fx.carton91@gmail.com>
2018-03-16 20:00:51 +0100

    Print only filename

--- test/scripts/test_png.sh
+++ test/scripts/test_png.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")/.." || die "wrong directory"
 mkdir -p out
 
 for png in assets/png/*.png; do
-    testing "$png"
+    testing "$(basename "$png")"
     ./png_rw "$png" "out/${png##*/}" 2>&- || fail "png_rw"
     ./scripts/cmp_img.sh "$png" "out/${png##*/}" 2>&- && pass || fail "cmp_img"
 done
