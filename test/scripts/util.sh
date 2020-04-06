e4178450da757f7b0600595cfd2d6b0f669c0298
François-Xavier Carton <fx.carton91@gmail.com>
2020-04-07 00:30:25 +0200

    tests: improve CI output

--- test/scripts/util.sh
+++ test/scripts/util.sh
@@ -15,18 +15,18 @@ testing() {
 
 fail() {
     if [ -n "$TEST_VERBOSE" ]; then
-        printf '\e[1m[%s] \e[31mFAIL \e[0;31m%s\e[0m\n' "$(basename "$0" .sh)" "$1" >/dev/tty
+        printf '\e[1m[%s] \e[31mFAIL \e[0;31m%s\e[0m\n' "$(basename "$0" .sh)" "$1" >&3
     else
-        printf '\e[1;31mFAIL \e[0;31m%s\e[0m\n' "$1" >/dev/tty
+        printf '\e[1;31mFAIL \e[0;31m%s\e[0m\n' "$1" >&3
     fi
     exit 1
 }
 
 pass() {
     if [ -n "$TEST_VERBOSE" ]; then
-        printf '\e[1m[%s] \e[32mPASS\e[0m\n' "$(basename "$0" .sh)" >/dev/tty
+        printf '\e[1m[%s] \e[32mPASS\e[0m\n' "$(basename "$0" .sh)" >&3
     else
-        printf '\e[1;32mPASS\e[0m\n' >/dev/tty
+        printf '\e[1;32mPASS\e[0m\n' >&3
     fi
     true
 }
@@ -38,8 +38,8 @@ assets() {
 do_test() {
     testing "$1"
     if [ -n "$TEST_VERBOSE" ]; then
-        (. /dev/stdin) && pass || code=1
+        (. /dev/stdin) 3>&1 && pass 3>&1 || code=1
     else
-        (. /dev/stdin) 1>/dev/null 2>/dev/null && pass || code=1
+        (. /dev/stdin) 3>&1 1>/dev/null 2>/dev/null && pass 3>&1 || code=1
     fi
 }
