7d19c2a132b68ab7907e85c5b23609cf144f086c
Sylvain Gauthier <syg@pedantic.software>
2021-04-05 08:40:44 +1000

    gltf: add support for jpeg

--- src/gltf/texture.c
+++ src/gltf/texture.c
@@ -5,22 +5,53 @@
 #include "gltf.h"
 #include "utils.h"
 
-static int parse_img_file(struct GltfContext* context, GLuint* tex, json_t* juri) {
-    char* pngPath;
+enum ImgMime {
+    IMG_MIME_PNG,
+    IMG_MIME_JPEG,
+    IMG_MIME_UNDEF
+};
 
-    if (!(pngPath = fullpath(context->path, json_string_value(juri)))) {
+static enum ImgMime infer_mime(const char* filename) {
+    const char* ptr;
+    if ((ptr = strrchr(filename, '.'))) {
+        if (!strcmp(ptr, ".png")) return IMG_MIME_PNG;
+        if (!strcmp(ptr, ".jpeg")) return IMG_MIME_JPEG;
+        if (!strcmp(ptr, ".JPEG")) return IMG_MIME_JPEG;
+        if (!strcmp(ptr, ".jpg")) return IMG_MIME_JPEG;
+        if (!strcmp(ptr, ".JPG")) return IMG_MIME_JPEG;
+    }
+    return IMG_MIME_UNDEF;
+}
+
+static int parse_img_file(struct GltfContext* context, GLuint* tex, json_t* juri, enum ImgMime mime) {
+    char* path;
+    GLuint (*texture_load)(const char*);
+
+    if (!(path = fullpath(context->path, json_string_value(juri)))) {
         return 0;
     }
-    if (!(*tex = texture_load_from_png(pngPath))) {
-        fprintf(stderr, "Error: gltf: texture: could not load texture: %s\n", pngPath);
-        free(pngPath);
+    if (mime == IMG_MIME_UNDEF) mime = infer_mime(path);
+    switch (mime) {
+        case IMG_MIME_PNG:
+            texture_load = texture_load_from_png;
+            break;
+        case IMG_MIME_JPEG:
+            texture_load = texture_load_from_jpeg;
+            break;
+        default:
+            fprintf(stderr, "Error: gltf: texture: can't infer mime type for %s, unkown extension\n", path);
+            return 0;
+    }
+    if (!(*tex = texture_load(path))) {
+        fprintf(stderr, "Error: gltf: texture: could not load texture: %s\n", path);
+        free(path);
         return 0;
     }
-    free(pngPath);
+    free(path);
     return 1;
 }
 
-static int parse_img_buffer(struct GltfContext* context, GLuint* tex, json_t* jbufview) {
+static int parse_img_buffer(struct GltfContext* context, GLuint* tex, json_t* jbufview, enum ImgMime mime) {
     const char* data;
     unsigned int bufview;
     struct GltfBufferView* view;
@@ -31,28 +62,48 @@ static int parse_img_buffer(struct GltfContext* context, GLuint* tex, json_t* jb
     }
     view = &context->bufferViews[bufview];
     data = ((char*)context->buffers[view->buffer].data) + view->byteOffset;
-    if (!(*tex = texture_load_from_png_buffer(data, view->byteLength))) {
-        fprintf(stderr, "Error: gltf: texture: could not load texture from buffer\n");
-        return 0;
+    switch (mime) {
+        case IMG_MIME_PNG:
+            if (!(*tex = texture_load_from_png_buffer(data, view->byteLength))) {
+                fprintf(stderr, "Error: gltf: texture: could not load texture from buffer\n");
+                return 0;
+            }
+            break;
+        case IMG_MIME_JPEG:
+            if (!(*tex = texture_load_from_jpeg_buffer(data, view->byteLength))) {
+                fprintf(stderr, "Error: gltf: texture: could not load texture from buffer\n");
+                return 0;
+            }
+            break;
+        default:
+            fprintf(stderr, "Error: gltf: texture: missing mime type, required for buffer data\n");
+            return 0;
     }
     return 1;
 }
 
 static int parse_img(struct GltfContext* context, GLuint* tex, json_t* jimg) {
     json_t* tmp;
-    char pngMime = 0;
+    enum ImgMime mime = IMG_MIME_UNDEF;
 
     if ((tmp = json_object_get(jimg, "mimeType"))) {
-        if (!json_string_value(tmp) || strcmp(json_string_value(tmp), "image/png")) {
-            fprintf(stderr, "Error: gltf: texture: invalid mimeType (only png supported)\n");
+        if (!json_string_value(tmp)) {
+            fprintf(stderr, "Error: gltf: texture: invalid mimeType.\n");
             return 0;
         }
-        pngMime = 1;
+        if (!strcmp(json_string_value(tmp), "image/png")) {
+            mime = IMG_MIME_PNG;
+        } else if (!strcmp(json_string_value(tmp), "image/jpeg")) {
+            mime = IMG_MIME_JPEG;
+        } else {
+            fprintf(stderr, "Error: gltf: texture: unsupported mimeType: %s\n", json_string_value(tmp));
+            return 0;
+        }
     }
     if ((tmp = json_object_get(jimg, "uri"))) {
         switch (gltf_uri_type(tmp)) {
             case GLTF_URI_FILE:
-                return parse_img_file(context, tex, tmp);
+                return parse_img_file(context, tex, tmp, mime);
             case GLTF_URI_B64:
                 fprintf(stderr, "Error: gltf: texture: base64 URIs for textures not implemented yet\n");
                 return 0;
@@ -61,11 +112,7 @@ static int parse_img(struct GltfContext* context, GLuint* tex, json_t* jimg) {
                 return 0;
         }
     } else if ((tmp = json_object_get(jimg, "bufferView"))) {
-        if (!pngMime) {
-            fprintf(stderr, "Error: gltf: texture: missing mime type, required for buffer data\n");
-            return 0;
-        }
-        return parse_img_buffer(context, tex, tmp);
+        return parse_img_buffer(context, tex, tmp, mime);
     }
     return 1;
 }
