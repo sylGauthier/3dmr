2d2243cca09a437d9d9e74a53cf7bceaa001e4a1
Sylvain Gauthier <syg@pedantic.software>
2021-04-10 11:54:43 +1000

    jpeg: add solid texture test to make test

--- test/scripts/test_solid.sh
+++ test/scripts/test_solid.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")/.." || die "wrong directory"
 [ -f test_scene ] || die "wrong directory"
 mkdir -p out
 
-assets rgb_tux.png tux.png || die "assets"
+assets rgb_tux.png tux.png tux.jpeg || die "assets"
 
 do_test "small cube" <<END
     ./test_scene solid_color_small "out/test_solid_color.png" || fail "screenshot"
@@ -20,10 +20,16 @@ do_test "big cube" <<END
     ./scripts/cmp_img.sh "out/test_solid_color_big.png" "out/expected_solid_color_big.png" || fail "cmp_img"
 END
 
-do_test "small cube - tux" <<END
+do_test "small cube - tux png" <<END
     ./test_scene solid_texture "out/test_solid_texture.png" || fail "screenshot"
     convert assets/tux.png -interpolate Nearest -filter point -geometry '94x94!' -flatten -gravity center -background black -extent 640x480 "out/expected_solid_texture.png" || fail "gen expected"
     ./scripts/cmp_img.sh -t 5 "out/test_solid_texture.png" "out/expected_solid_texture.png" || fail "cmp_img"
+END
+
+do_test "small cube - tux jpeg" <<END
+    ./test_scene solid_jpeg_texture "out/test_solid_jpeg_texture.png" || fail "screenshot"
+    convert assets/tux.jpeg -interpolate Nearest -filter point -geometry '94x94!' -flatten -gravity center -background black -extent 640x480 "out/expected_solid_jpeg_texture.png" || fail "gen expected"
+    ./scripts/cmp_img.sh -t 5 "out/test_solid_jpeg_texture.png" "out/expected_solid_jpeg_texture.png" || fail "cmp_img"
 END
 
 exit "$code"
